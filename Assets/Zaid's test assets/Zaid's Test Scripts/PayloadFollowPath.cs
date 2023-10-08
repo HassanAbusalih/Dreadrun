efb7c4a380eb0c15a96580b8dc2cece3aa0c7cc2
T3NSH11 <m.zaid@student.saedubai.com>
2023-10-08 21:22:30 +0400

    Updates to Payload system
    - Payload now detects players using OverlapSphere.
    - Multiple speeds can now be set for number of players on the payload.
    - Payload now has a reverse countdown after which it starts going backwards on the path.
    - Created EXP particle system for the payload phase to show the payload "absorbing?" the EXP (Extra).

--- Assets/Zaid's test assets/Zaid's Test Scripts/PayloadFollowPath.cs
+++ Assets/Zaid's test assets/Zaid's Test Scripts/PayloadFollowPath.cs
@@ -1,38 +1,90 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using UnityEngine;
 
 public class PayloadFollowPath : MonoBehaviour
 {
+    [Header("Path Follow Settings")]
     public PayloadPath currentPath;
     public int currentNodeID = 0;
-    public float Speed = 5f;
-    public float RotationSpeed = 5f;
-    public float WayPointSize = 1f;
+    public float wayPointSize;
 
-    public GameObject player;
-    public float payloadMoveRange;
+    [Header("Speed Settings")]
+    public float onePlayerSpeed;
+    public float twoPlayerSpeed;
+    public float threePlayerSpeed;
+    public float reverseSpeed;
+    public float rotationSpeed;
+    private float speed;
+
+    [Header("Reverse Settings")]
+    public float reverseCountDownTime;
+    private float reverseTimer;
+
+    [Header("Player Detection Settings")]
+    public float payloadRange;
+    public LayerMask playerLayer;
+    private int playersOnPayloadCount;
 
 
     private void Start()
     {
+        reverseTimer = reverseCountDownTime;
         currentPath = GameObject.FindGameObjectWithTag("PayloadPath").GetComponent<PayloadPath>();
-        player = GameObject.FindGameObjectWithTag("Player");
     }
     private void Update()
     {
-        if (Vector3.Distance(player.transform.position, transform.position) < payloadMoveRange && currentNodeID < currentPath.pathNodes.Count)
+        Collider[] playersOnPayload = Physics.OverlapSphere(transform.position, payloadRange, playerLayer);
+        playersOnPayloadCount = playersOnPayload.Length;
+
+        if (playersOnPayloadCount > 0 && currentNodeID < currentPath.pathNodes.Count)
         {
+            reverseTimer = reverseCountDownTime;
+
+            if (playersOnPayloadCount == 1)
+            {
+                speed = onePlayerSpeed;
+            }
+            else if (playersOnPayloadCount == 2)
+            {
+                speed = twoPlayerSpeed;
+            }
+            else if (playersOnPayloadCount == 3)
+            {
+                speed = threePlayerSpeed;
+            }
+
             float node_Distance = Vector3.Distance(currentPath.pathNodes[currentNodeID].position, transform.position);
-            transform.position = Vector3.MoveTowards(transform.position, currentPath.pathNodes[currentNodeID].position, Time.deltaTime * Speed);
+            transform.position = Vector3.MoveTowards(transform.position, currentPath.pathNodes[currentNodeID].position, Time.deltaTime * speed);
 
             Quaternion target_Rotation = Quaternion.LookRotation(currentPath.pathNodes[currentNodeID].position - transform.position);
-            transform.rotation = Quaternion.Slerp(transform.rotation, target_Rotation, Time.deltaTime * RotationSpeed);
+            transform.rotation = Quaternion.Slerp(transform.rotation, target_Rotation, Time.deltaTime * rotationSpeed);
 
-            if (node_Distance <= WayPointSize)
+            if (node_Distance <= wayPointSize)
             {
                 currentNodeID++;
+            }
+        }
+        else
+        {
+            reverseTimer -= Time.deltaTime;
+
+            if (reverseTimer <= 0 && currentNodeID > 0)
+            {
+                speed = reverseSpeed;
+
+                float node_Distance = Vector3.Distance(currentPath.pathNodes[currentNodeID - 1].position, transform.position);
+                transform.position = Vector3.MoveTowards(transform.position, currentPath.pathNodes[currentNodeID - 1].position, Time.deltaTime * speed);
+
+                Quaternion target_Rotation = Quaternion.LookRotation(currentPath.pathNodes[currentNodeID].position - transform.position);
+                transform.rotation = Quaternion.Slerp(transform.rotation, target_Rotation, Time.deltaTime * rotationSpeed);
+
+                if (node_Distance <= wayPointSize)
+                {
+                    currentNodeID--;
+                }
             }
         }
     }
