8a7bd074faa5a0b05898303045381e3987bfefb4
Armaantakyar1 <102411631+Armaantakyar1@users.noreply.github.com>
2023-10-12 14:10:58 +0400

    stats class

---
+++ Assets/Scripts/Player/PlayerStats.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStats
+{
+    public float health;
+    public float attack;
+    public float stamina;
+}
