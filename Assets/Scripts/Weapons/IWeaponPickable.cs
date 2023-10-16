97dda381a478e571fd6f42d4105caee8df68a8d6
Avi_156 <avi.g751@gmail.com>
2023-10-17 00:34:33 +0400

    Weopens are now pickable, when picked up, weapon id is stored in player

---
+++ Assets/Scripts/Weapons/IWeaponPickable.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface  IWeaponPickable 
+{
+    public void PickUpWeapon(Transform _weaponEquipPosition,ref  int _ID)
+    {
+
+    }
+}
