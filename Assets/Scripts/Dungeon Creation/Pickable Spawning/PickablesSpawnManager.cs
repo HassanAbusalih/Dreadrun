2383419fdf8ea804821a694a96661c4cc8a46616
Avi_156 <avi.g751@gmail.com>
2023-10-18 15:33:30 +0400

    random weapon spawnign done

--- Assets/Scripts/Dungeon Creation/Pickable Spawning/PickablesSpawnManager.cs
+++ Assets/Scripts/Dungeon Creation/Pickable Spawning/PickablesSpawnManager.cs
@@ -7,17 +7,17 @@ public class PickablesSpawnManager : MonoBehaviour
 {
     [Header("Weapon Spawn Settings")]
     [SerializeField] int amountOfWeaponsToSpawn;
-    [SerializeField] private GameObject[] weaponPrefabs;
-   
+    [SerializeField] private List<GameObject> weaponPrefabs;
+
     [Header("Consumable Spawn Settings")]
     [SerializeField] int amountOfConsumablesToSpawn;
-    [SerializeField] private GameObject[] consumablePrefabs;
+    [SerializeField] private List<GameObject> consumablePrefabs;
 
     [Header("Exp Orbs  Spawn Settings")]
     [Tooltip("Exp amount is the actual total value of exp it has to spawn based on given spawn points")]
     [SerializeField] int amountOfExpToSpawn = 500;
     [SerializeField] private GameObject[] expOrbPrefabs;
-   
+
 
     [Header("Exp Orb Value Types")]
     [SerializeField] int lowExpOrbValue = 10;
@@ -36,17 +36,40 @@ public class PickablesSpawnManager : MonoBehaviour
 
     private void Start()
     {
-        InitializeAllExpOrbValueTypesDictionary();
-        StartTheProcessOfSpawningAllPickablesAtRandomSpawnPoints();
+        InitializeExpOrbTypesDictionary();
+        SpawnAllPickables();
     }
 
-    private void StartTheProcessOfSpawningAllPickablesAtRandomSpawnPoints()
+    private void SpawnAllPickables()
     {
+        SpawnAllWeapons();
         SpawnAllExpOrbs();
+    }
+
+    void SpawnAllWeapons()
+    {
+        if (weaponPrefabs.Count == 0) { return; }
+        for (numberOfWeaponsSpawned = 0; numberOfWeaponsSpawned < amountOfWeaponsToSpawn; numberOfWeaponsSpawned++)
+        {
+            if (weaponPrefabs.Count == 0) { return; } // if there are no more weapons to spawn, then return
+            int _randomWeaponIndex = Random.Range(0, weaponPrefabs.Count);
+            GameObject weaponToSpawn = weaponPrefabs[_randomWeaponIndex];
+
+            bool _isAbleToSpawn = SpawnAPickableAtRandomSpawnPoint(weaponToSpawn);
+            if (!_isAbleToSpawn) return;
+
+            weaponPrefabs.RemoveAt(_randomWeaponIndex);
+        }
+    }
+
+    void SpawnAllConsumables()
+    {
+
     }
 
     void SpawnAllExpOrbs()
     {
+        if (expOrbPrefabs.Length == 0) { return; }
         for (numberOfExpOrbsSpawned = 0; totalExpSpawnedInSoFar < amountOfExpToSpawn;)
         {
             int _probabilityToSpawnExpOrb = Random.Range(0, 10);
@@ -55,23 +78,11 @@ public class PickablesSpawnManager : MonoBehaviour
             GameObject expOrbTypeToSpawn = expOrbPrefabs[Random.Range(0, expOrbPrefabs.Length)];
             int _expOrbValueToSpawn = allExpOrbValueTypesDictionary[expOrbTypeToSpawn];
 
-            SpawnAPickableAtRandomSpawnPoint(expOrbTypeToSpawn);
+            bool _isAbleToSpawn = SpawnAPickableAtRandomSpawnPoint(expOrbTypeToSpawn);
+            if (!_isAbleToSpawn) return;
+
             UpdateExpOrbSpawningStats(_expOrbValueToSpawn);
         }
-    }
-
-    void SpawnAPickableAtRandomSpawnPoint(GameObject _pickableToSpawn)
-    {
-        // if there are no spawn points left, then return (this is to avoid index out of range error)
-        if (AllPickableSpawnPoints.Count == 0)
-        { Debug.LogWarning("There is no more pickable spawn points,so cannot spawn more, try changing spawn settings "); return; } 
-
-        int _randomPickableSpawnPointIndex = Random.Range(0, AllPickableSpawnPoints.Count);
-        Vector3 _randomSpawnPosition = AllPickableSpawnPoints[_randomPickableSpawnPointIndex].position;
-
-        GameObject _pickableSpawned = Instantiate(_pickableToSpawn, _randomSpawnPosition, Quaternion.identity);
-        _pickableSpawned.transform.parent = this.transform;
-        AllPickableSpawnPoints.RemoveAt(_randomPickableSpawnPointIndex);
     }
 
     void UpdateExpOrbSpawningStats(int _expValue)
@@ -79,12 +90,28 @@ public class PickablesSpawnManager : MonoBehaviour
         totalExpSpawnedInSoFar += _expValue;
         numberOfExpOrbsSpawned++;
     }
-    private void InitializeAllExpOrbValueTypesDictionary()
+
+    bool SpawnAPickableAtRandomSpawnPoint(GameObject _pickableToSpawn)
+    {
+        // if there are no spawn points left, then return (this is to avoid index out of range error)
+        if (AllPickableSpawnPoints.Count == 0)
+        { Debug.LogWarning("There is no more pickable spawn points,so cannot spawn more, try changing spawn settings "); return false; }
+
+        int _randomPickableSpawnPointIndex = Random.Range(0, AllPickableSpawnPoints.Count);
+        Vector3 _randomSpawnPosition = AllPickableSpawnPoints[_randomPickableSpawnPointIndex].position;
+
+        GameObject _pickableSpawned = Instantiate(_pickableToSpawn, _randomSpawnPosition, Quaternion.identity);
+        _pickableSpawned.transform.parent = this.transform;
+        AllPickableSpawnPoints.RemoveAt(_randomPickableSpawnPointIndex);
+        return true;
+    }
+
+    private void InitializeExpOrbTypesDictionary()
     {
         if (expOrbPrefabs.Length == 0) { return; }
 
         allExpOrbValueTypesDictionary.Add(expOrbPrefabs[0], lowExpOrbValue);
         allExpOrbValueTypesDictionary.Add(expOrbPrefabs[1], mediumExpOrbValue);
-        allExpOrbValueTypesDictionary.Add(expOrbPrefabs[2], highExpOrbValue);    
+        allExpOrbValueTypesDictionary.Add(expOrbPrefabs[2], highExpOrbValue);
     }
 }
