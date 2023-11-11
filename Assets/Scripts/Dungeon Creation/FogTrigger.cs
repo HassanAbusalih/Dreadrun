3b8abcaa56f81d5aeef2d6978a967a06e13aafd9
Avinash <avi.g751@gmail.com>
2023-11-11 17:56:28 +0400

    screw particle systems

--- Assets/Scripts/Dungeon Creation/FogTrigger.cs
+++ Assets/Scripts/Dungeon Creation/FogTrigger.cs
@@ -6,6 +6,7 @@ public class FogTrigger : MonoBehaviour
     [Header("Fog Particle Settings")]
     [SerializeField] Vector2 fogBoxScale;
     [SerializeField] float particleEmissionRate;
+    [SerializeField] float particleLifetime;
     [SerializeField] Color fogColor;
 
     [Header("Fog Trigger Settings")]
@@ -14,6 +15,7 @@ public class FogTrigger : MonoBehaviour
     [SerializeField] bool destroyAfterFadeOut;
     
     ParticleSystem fog;
+    ParticleSystemRenderer fogRenderer;
     BoxCollider boxCollider;
     float delayToDestroy;
     float elapsedTime;
@@ -22,12 +24,15 @@ public class FogTrigger : MonoBehaviour
     void Start()
     {
         delayToDestroy = fadeOutDuration + 4f;
+        fog.time = 0.01f;
+        fog.Play();
     }
 
     private void OnValidate()
     {
         fog = GetComponentInChildren<ParticleSystem>();
         boxCollider = GetComponent<BoxCollider>();
+        fogRenderer = fog.GetComponent<ParticleSystemRenderer>();
         boxCollider.isTrigger = true;
 
         var fogParticleEmission = fog.emission;
@@ -36,6 +41,9 @@ public class FogTrigger : MonoBehaviour
         boxCollider.size = new Vector3(fogBoxScale.x, 1, fogBoxScale.y);
         var fogParticleShape = fog.shape;
         fogParticleShape.scale = new Vector3(fogBoxScale.x, fogBoxScale.y, 0);
+
+        var lifeTime = fog.main;
+        lifeTime.startLifetime= particleLifetime;
 
         var fogMain = fog.main;
         fogMain.startColor = fogColor;
@@ -64,10 +72,18 @@ public class FogTrigger : MonoBehaviour
         if (!startFading) return;
         if (elapsedTime < fadeOutDuration)
         {
+
             elapsedTime += Time.deltaTime;
             var _emission = fog.emission;
             float _emissionRate = _emission.rateOverTime.constant;
+            var _lifetime = fog.main;
+            var _color = fog.main;
+
+            _lifetime.startLifetime = Mathf.Lerp(_lifetime.startLifetime.constant, 0, elapsedTime / fadeOutDuration);
             _emission.rateOverTime = Mathf.Lerp(_emissionRate, 0, elapsedTime / fadeOutDuration);
+            _color.startColor = Color.Lerp(_color.startColor.color, Color.clear, elapsedTime / fadeOutDuration);
+            fogRenderer.maxParticleSize = Mathf.Lerp(fogRenderer.maxParticleSize, 0, elapsedTime / fadeOutDuration);
+            
             return;
         }
         DestroyAfterFadingOut();
