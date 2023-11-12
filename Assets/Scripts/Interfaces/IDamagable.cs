980d8f160369f01c48cdea33524a5662d9fe1193
Avinash <avi.g751@gmail.com>
2023-11-13 01:52:39 +0400

    when enemy or player takes damage color and scale changes

--- Assets/Scripts/Interfaces/IDamagable.cs
+++ Assets/Scripts/Interfaces/IDamagable.cs
@@ -1,7 +1,9 @@
+using System;
 using UnityEngine;
 
 public interface IDamagable 
 {
+    public static Action<GameObject> onDamageTaken;
     public void TakeDamage(float Amount);
     public GameObject gameObject { get; }
 }
