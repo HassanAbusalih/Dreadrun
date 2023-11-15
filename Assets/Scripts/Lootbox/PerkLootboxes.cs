3f685b0353c94778512e90af20992cdab488f09e
Armaantakyar1 <102411631+Armaantakyar1@users.noreply.github.com>
2023-11-15 19:24:35 +0400

    lootboxes working with saving

---
+++ Assets/Scripts/Lootbox/PerkLootboxes.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PerkLootboxes : MonoBehaviour
+{
+    [SerializeField] List<Perk> unlockablePerkPool;
+    [SerializeField] bool opened;
+    [SerializeField] bool entered;
+    [SerializeField] UnlockedPerks unlocked;
+    // Start is called before the first frame update
+    void Start()
+    {
+        unlocked = FindObjectOfType<UnlockedPerks>();
+        RemoveCommonPerks(unlockablePerkPool, unlocked.unlockedPerkpool);
+    }
+
+    void RemoveCommonPerks(List<Perk> unlockable, List<Perk> unlocked)
+    {
+        unlockable.RemoveAll(poolA => unlocked.Contains(poolA));
+        if (unlockablePerkPool.Count == 0)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    void Update()
+    {
+        if (entered)
+        {
+            OpenLootBox();
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<UnlockedPerks>())
+        {
+            entered = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        entered = false;
+    }
+
+    void OpenLootBox()
+    {
+        if(Input.GetKeyDown(KeyCode.E)&& !opened)
+        {
+            int randomPerk = Random.Range(0, unlockablePerkPool.Count);
+            Perk selectedPerk = unlockablePerkPool[randomPerk];
+            unlocked.AddNewPerk(selectedPerk);
+            opened = true;
+        }
+    }
+}
---
+++ Assets/Scripts/Lootbox/UnlockedPerks.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class UnlockedPerks : MonoBehaviour
+{
+    public List<Perk> unlockedPerkpool;
+
+    public void SavePerks()
+    {
+        string jsonData = JsonUtility.ToJson(new PerkData(unlockedPerkpool));
+        File.WriteAllText("perks.json", jsonData);
+        Debug.Log("Perks saved to JSON.");
+    }
+
+    void Start()
+    {
+
+        LoadPerks();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.U))
+        {
+            SavePerks();
+        }
+    }
+
+    public void AddNewPerk(Perk newPerk)
+    {
+        unlockedPerkpool.Add(newPerk);
+    }
+
+    private void LoadPerks()
+    {
+        if (File.Exists("perks.json"))
+        {
+            string jsonData = File.ReadAllText("perks.json");
+            PerkData perkData = JsonUtility.FromJson<PerkData>(jsonData);
+
+            unlockedPerkpool = perkData.unlockedPerks;
+            Debug.Log("Perks read to JSON.");
+        }
+    }
+
+    [System.Serializable]
+    private class PerkData
+    {
+        public List<Perk> unlockedPerks;
+
+        public PerkData(List<Perk> perks)
+        {
+            unlockedPerks = perks;
+        }
+    }
+}
