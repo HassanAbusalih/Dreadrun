0341216524a6ac56d993540ead306b5bae9845eb
somnolentz <92248002+somnolentz@users.noreply.github.com>
2023-11-28 15:51:01 +0400

    script repush

--- Assets/Scripts/Player/InstaRespawnHazard.cs
+++ Assets/Scripts/Player/InstaRespawnHazard.cs
@@ -14,6 +14,7 @@ public class InstaRespawnHazard : MonoBehaviour
             if (playerRespawn != null)
             {
                 playerRespawn.Respawn();
+                //repush
             }
         }
     }
