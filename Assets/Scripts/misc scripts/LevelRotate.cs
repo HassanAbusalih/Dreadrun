1b28d2f34ed2f98c4d831bf22000e007a2a7c9a8
somnolentz <92248002+somnolentz@users.noreply.github.com>
2023-12-05 17:09:25 +0400

    LEVEL ROTATIONS (BIG BRAIN)

---
+++ Assets/Scripts/misc scripts/LevelRotate.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class LevelRotate: MonoBehaviour
+{
+    // Define the possible X scales and Y rotations
+    private float[] xScales = { -1f, 1f };
+    private float[] yRotations = { 90f, -90f, 180f, -180f };
+
+    void Start()
+    {
+        Transform levelTransform = transform;
+        float selectedXScale = xScales[Random.Range(0, xScales.Length)];
+        float selectedYRotation = yRotations[Random.Range(0, yRotations.Length)];
+
+        levelTransform.localScale = new Vector3(selectedXScale, levelTransform.localScale.y, levelTransform.localScale.z);
+        levelTransform.localRotation = Quaternion.Euler(0f, selectedYRotation, 0f);
+    }
+}
