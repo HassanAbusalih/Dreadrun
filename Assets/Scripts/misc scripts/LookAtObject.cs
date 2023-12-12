b928f17b6ea95828b288cae1dfb95b2bd5e4113d
Avinash <avi.g751@gmail.com>
2023-12-12 20:41:06 +0400

    why are rotattions soo cursed ???? :(

---
+++ Assets/Scripts/misc scripts/LookAtObject.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtObject : MonoBehaviour
+{
+    [SerializeField] Transform objectToLookAt;
+    public bool invertLookAt;
+    Vector3 startRotation;
+
+    private void Start()
+    {
+        if(invertLookAt)
+        startRotation = -transform.rotation.eulerAngles;
+        else startRotation = transform.rotation.eulerAngles;
+    }
+
+    private void Update()
+    {
+        Vector3 direction = (objectToLookAt.position - transform.position).normalized;
+        direction.y = 0; // this for some reason allows the y to rotate, it makes no sense but it works
+        Quaternion _lookAtRotation = Quaternion.LookRotation(direction);
+        
+
+
+        if (invertLookAt)
+        {
+           transform.rotation = Quaternion.LookRotation(-direction) * Quaternion.Euler(startRotation);
+        }
+        else
+        {
+            transform.rotation =  Quaternion.LookRotation(direction) * Quaternion.Euler(startRotation);
+        }
+    }
+}
