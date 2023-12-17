3af10e89ca416525fa4a81f4de52649bfbaeb5fc
Prieance <p.sagar@saedubai.com>
2023-12-18 00:00:05 +0400

    Monetization for Characters
    -Store credit added for purchasing skin
    -Texts for when skin is purchased

--- Assets/Scripts/Menu System/CharacterCustomizationMenu.cs
+++ Assets/Scripts/Menu System/CharacterCustomizationMenu.cs
@@ -5,22 +5,34 @@ public class CharacterCustomizationMenu : MonoBehaviour
     [SerializeField] MeshRenderer character;
     [SerializeField] Material[] characterSkins;
     [SerializeField] int currentSkin;
+    [SerializeField] GameObject purchaseOption;
 
     public void NextSkin()
     {
         currentSkin = (currentSkin + 1) % characterSkins.Length;
         character.material = characterSkins[currentSkin];
+        purchaseOption.SetActive(true);
+
+        if (currentSkin == 0)
+        {
+            purchaseOption.SetActive(false);
+        }
     }
 
     public void PreviousSkin()
     {
         currentSkin--;
+        purchaseOption.SetActive(true);
 
         if (currentSkin < 0)
         {
             currentSkin += characterSkins.Length;
         }
 
+        if(currentSkin == 0)
+        {
+            purchaseOption.SetActive(false);
+        }
         character.material = characterSkins[currentSkin];
     }
 }
---
+++ Assets/Scripts/Monetization/ItemPurchase.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemPurchase : MonoBehaviour
+{
+    [SerializeField] Text coins;
+    [SerializeField] int amount;
+    [SerializeField] GameObject purchaseComplete;
+    [SerializeField] GameObject purchased;
+    // Start is called before the first frame update
+    private void Start()
+    {
+        coins = GameObject.Find("moneyAmount").GetComponent<Text>();
+        purchaseComplete.SetActive(false);
+    }
+
+    public void Purchase()
+    {
+        amount = 0;
+        coins.text = "" + amount;
+        purchaseComplete.SetActive(true);
+        purchased.SetActive(false);
+    }
+
+}
