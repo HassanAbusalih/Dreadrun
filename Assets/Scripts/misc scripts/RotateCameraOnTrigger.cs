a0dd9c215a8cba8be4c7d0265d0682c91b56839f
Avinash <avi.g751@gmail.com>
2024-01-18 00:06:36 +0400

    I hate camera shit so bad and why is our quality settings set to low

--- Assets/Scripts/misc scripts/RotateCameraOnTrigger.cs
+++ Assets/Scripts/misc scripts/RotateCameraOnTrigger.cs
@@ -7,20 +7,39 @@ using Cinemachine;
 public class RotateCameraOnTrigger : MonoBehaviour
 {
     [SerializeField] Transform camFollow;
-    [SerializeField] CinemachineVirtualCamera mainVirtualCam;
     [Header("Camera Rotation Settings")]
     [SerializeField] bool useTriggerAsAngleOffset;
+    [SerializeField] bool useCamStartAngleAsExit = true;
     [SerializeField] float angleOffset;
+    
     [SerializeField] float rotateDuration;
     [SerializeField] MinMaxCurve rotationCurve;
 
     [Header("Debug Stats")]
-    [SerializeField] float previousRotationAngle;
+    [SerializeField] float? previousRotationAngle = null;
     [SerializeField] float AlignmentBetweenCamAndTrigger;
+    [SerializeField] float startAngle;
     float previosuXDamping;
 
     [SerializeField] bool shouldDestroy;
 
+
+    private void Start()
+    {
+        if (camFollow == null)
+        {
+            camFollow = FindObjectOfType<CameraFocus>().transform;
+        }
+
+        Invoke("GetStartAngle", 1.5f);
+     
+
+    }
+
+    void GetStartAngle()
+    {
+        startAngle = camFollow.transform.eulerAngles.y;
+    }
 
     private void OnValidate()
     {
@@ -31,11 +50,6 @@ public class RotateCameraOnTrigger : MonoBehaviour
         }
     }
 
-    private void Start()
-    {
-        previosuXDamping = mainVirtualCam.GetCinemachineComponent<CinemachineTransposer>().m_XDamping;
-    }
-
     private void OnTriggerExit(Collider other)
     {
         if (camFollow == null) return;
@@ -44,9 +58,8 @@ public class RotateCameraOnTrigger : MonoBehaviour
             Vector3 directionFromCamera = (camFollow.transform.position - transform.position).normalized;
             AlignmentBetweenCamAndTrigger = Vector3.Dot(transform.forward, directionFromCamera);
             StopAllCoroutines();
-            mainVirtualCam.GetCinemachineComponent<CinemachineTransposer>().m_XDamping = 0;
-
-            if (AlignmentBetweenCamAndTrigger > 0.015f) // you never know when these stupid edge casses happen so i just put a small value above 0
+           
+            if (AlignmentBetweenCamAndTrigger > 0.1f && previousRotationAngle==null ) // you never know when these stupid edge casses happen so i just put a small value above 0
             {
                 previousRotationAngle = camFollow.transform.eulerAngles.y;
                 float targetAngleOffset = useTriggerAsAngleOffset ? transform.rotation.eulerAngles.y : angleOffset;
@@ -57,30 +70,29 @@ public class RotateCameraOnTrigger : MonoBehaviour
                 }
                 return;
             }
-            StartCoroutine(RotateCamera(previousRotationAngle));
+            float resetAngle = useCamStartAngleAsExit ? startAngle : (float)previousRotationAngle;
+            StartCoroutine(RotateCamera(resetAngle));
+            previousRotationAngle = null;
 
         }
 
     }
 
 
-    void ResetCameraDamping()
-    {
-        mainVirtualCam.GetCinemachineComponent<CinemachineTransposer>().m_XDamping = previosuXDamping;
-    }
+  
 
-    IEnumerator RotateCamera(float angle)
+    IEnumerator RotateCamera(float? angle)
     {
         float timer = 0;
-        float randomValue = Random.value;
+        float randomValue = rotationCurve.mode == ParticleSystemCurveMode.TwoConstants ? Random.value : 0;
         float currentAngle = camFollow.transform.eulerAngles.y;
+      
         while (timer < rotateDuration)
         {
             timer += Time.deltaTime;
             float t = rotationCurve.Evaluate(timer / rotateDuration, randomValue);
-            camFollow.transform.eulerAngles = new Vector3(camFollow.transform.eulerAngles.x, Mathf.LerpAngle(currentAngle, angle, t), camFollow.transform.eulerAngles.z);
+            camFollow.transform.eulerAngles = new Vector3(camFollow.transform.eulerAngles.x, Mathf.LerpAngle(currentAngle, (float)angle, t), camFollow.transform.eulerAngles.z);
             yield return null;
         }
-        Invoke(nameof(ResetCameraDamping), 1f);
     }
 }
